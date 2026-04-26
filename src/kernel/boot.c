76948b8b8e46740ce302a00cc9d502d61bcf911a
officialf1zzy <fuzzt000@gmail.com>
2026-04-26 20:35:14 +0200

    Fix formatting and include statement in boot.c

--- src/kernel/boot.c
+++ src/kernel/boot.c
@@ -2,11 +2,10 @@
 
 __attribute__((section(".multiboot"), used))
 static const uint32_t multiboot_header[] = {
-    0xE85250D6,                 // magic
-    0,                          // architecture (i386 = 0)
-    16,                         // header length
-    -(0xE85250D6 + 0 + 16),     // checksum
+    0xE85250D6,                 
+    0,                          
+    16,                         
+    -(0xE85250D6 + 0 + 16),     
 
-    // END TAG (required)
     0, 0, 8, 0
 };
